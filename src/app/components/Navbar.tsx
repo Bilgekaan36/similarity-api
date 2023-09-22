640e35747ad3a513fa973f59be6cfe773f06e949
Bilgekaan36 <b.yilmaz@workdigital.de>
2023-09-22 16:44:51 +0300

    [BY-1] add Button component and implement into navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,5 +1,7 @@
 import { getServerSession } from "next-auth";
+import Link from "next/link";
 import { FC } from "react";
+import { buttonVariants } from "./ui/Button";
 
 interface NavbarProps {}
 
@@ -7,7 +9,39 @@ const Navbar = async ({}) => {
   const session = await getServerSession();
   return (
     <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
-      <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'></div>
+      <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'>
+        <Link href='/' className={buttonVariants({ variant: "link" })}>
+          Text Similarity 1.0
+        </Link>
+
+        <div className='md:hidden'>
+          <ThemeToggle />
+        </div>
+
+        <div className='hidden md:flex gap-4'>
+          <ThemeToggle />
+          <Link
+            href='/documentation'
+            className={buttonVariants({ variant: "ghost" })}
+          >
+            Documentation
+          </Link>
+
+          {session ? (
+            <>
+              <Link
+                href='/dashboard'
+                className={buttonVariants({ variant: "ghost" })}
+              >
+                Dashboard
+              </Link>
+              <SignOutButton />
+            </>
+          ) : (
+            <SignInButton />
+          )}
+        </div>
+      </div>
     </div>
   );
 };
